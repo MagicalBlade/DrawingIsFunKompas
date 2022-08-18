0fb2400707e962f5d4cbd626ba82683f791adc5e
Александр Воскобойник <magical.blade@gmail.com>
2022-08-18 21:53:05 +0300

    парсинг размеров

---
+++ StaticClasses/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingIsFunKompas.StaticClasses
+{
+    internal static class Utilities
+    {
+        public static double[] ParsingDimensions(string strDimensions)
+        {
+            string[] dimensions = strDimensions.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            double[] result = new double[dimensions.Length];
+            for (int i = 0; i < dimensions.Length; i++)
+            {
+                try
+                {
+                    result[i] = double.Parse(dimensions[i]);
+
+                }
+                catch (FormatException)
+                {
+                    System.Windows.MessageBox.Show("Не верный формат");
+                }
+            }
+            return result;
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,36 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using U = DrawingIsFunKompas.StaticClasses.Utilities;
 
 namespace DrawingIsFunKompas.ViewModels
 {
-    internal class MainWindowViewModel : ObservableObject
+    internal partial class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string? _topDimensions;
+        [ObservableProperty]
+        private string? _bottomDimensions;
+        [ObservableProperty]
+        private string? _leftDimensions;
+        [ObservableProperty]
+        private string? _rightDimensions;
 
+        [ObservableProperty]
+        private string? _info;
+
+        [RelayCommand]
+        private void Drawing()
+        {
+            foreach (var item in U.ParsingDimensions(TopDimensions))
+            {
+                MessageBox.Show($"{item}");
+            }
+        }
     }
 }
